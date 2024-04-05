fcf8486492956b1a6cb0cb2f78a1717253b22661
이보미 <leebomi@ibomiui-MacBookAir.local>
2024-04-05 19:46:19 +0900

    검색바 모바일 사이즈 반응형 완성

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,9 +6,9 @@ function Nav({ profile }) {
     <div className="nav">
       <div className="wrap">
         <img className="logo-image" src="/images/logo.png" alt="logo" />
-        <div className="login-button">
+        {/* <div className="login-button">
           <p className="login-button-text">로그인</p>
-        </div>
+        </div> */}
         <Profile profile={profile} />
       </div>
     </div>
