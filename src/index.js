cb5819d9ccfebecc9d987cc73f341c8bbf4b06d1
이보미 <leebomi@ibomiui-MacBookAir.local>
2024-04-05 23:56:44 +0900

    검색바 테스트 성공

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Folder from "../src/components/pages/Folder";
+// import Folder from "../src/components/pages/Folder";
+import Shared from "./components/pages/Shared";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Folder />
+    {/* <Folder /> */}
+    <Shared />
   </React.StrictMode>
 );
