d1648a397d78d261d24a6cb234f8da49556c641a
이보미 <leebomi@ibomiui-MacBookAir.local>
2024-04-06 03:00:16 +0900

    FolderHeader.jsx , css 파일 완성

--- src/components/FolderHeader.jsx
+++ src/components/FolderHeader.jsx
@@ -1,8 +1,24 @@
+import "../css/FolderHeader.css";
+
 function FolderHeader() {
   return (
-    <div>
-      <div>
-        <div></div>
+    <div className="wrap-all-link-search-bar">
+      <div className="wrap-link-search-bar">
+        <div className="wrap-link-search-bar-2">
+          <input
+            className="link-search-bar"
+            type="text"
+            placeholder="링크를 추가해 보세요"
+          />
+          <img
+            className="link-img"
+            src="images/link.png"
+            alt="링크 추가 모양"
+          />
+          <div className="link-add-button">
+            <p className="link-add-button-text">추가하기</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import Folder from "../src/components/pages/Folder";
-import Shared from "./components/pages/Shared";
+import Folder from "../src/components/pages/Folder";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Folder /> */}
-    <Shared />
+    <Folder />
   </React.StrictMode>
 );
